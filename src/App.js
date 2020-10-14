82644bbadbee2b7496e904cc42af7ce0c5a9b124
clayton <clayton.anzalone@mnsu.edu>
2020-10-14 19:02:16 +0200

    Modifications

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { CssBaseline } from '@material-ui/core'
 import React from 'react'
 import './App.css'
 import TreeShowcase from './pages/treeShowcase'
@@ -5,6 +6,7 @@ import TreeShowcase from './pages/treeShowcase'
 function App () {
   return (
     <div className='App'>
+      <CssBaseline />
       <TreeShowcase />
     </div>
   )
--- src/pages/treeShowcase.js
+++ src/pages/treeShowcase.js
@@ -9,103 +9,114 @@ import CardMedia from '@material-ui/core/CardMedia'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import fetch from 'isomorphic-fetch'
-import _get from 'lodash'
+import { CircularProgress, Collapse } from '@material-ui/core'
+import { isArray } from 'lodash'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1
+    padding: 64
   },
-  tree_card: {
-    maxWidth: 600,
-    margins: 100
+  treeCard: {
+    maxWidth: 600
+  },
+  cardActions: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  fullScreenLoader: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    width: '100vw'
   }
-})
+}))
 
 const TreeShowcase = () => {
   const classes = useStyles()
   const [data, setData] = useState({ trees: [] })
-  const [showResults, setShowResults] = React.useState(false)
+  const [loading, setLoading] = React.useState(true)
+  const [error, setError] = useState(null)
 
-  const TreeData = () => {
-    useEffect(() => {
-      const fetchData = async () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
         const response = await fetch('https://s3.eu-central-1.amazonaws.com/ecosia-frontend-developer/trees.json')
         const data = await response.json()
-        console.log(data)
         setData(data)
+      } catch (error) {
+        setError(error)
+        console.error(error)
       }
-      fetchData()
-    }, [])
-    return data
-  }
-  TreeData()
+      setLoading(false)
+    }
+    fetchData()
+  }, [])
 
   const handleChange = (name) => {
     const trees = data.trees.map(tree => {
-      if (tree.name !== name) {
-      // Do an equivelency check here, if the tree names are equivelent show the tree, if not don't show it.
+      if (tree.name === name) {
         return { ...tree, hidden: !tree.hidden }
       }
       return tree
     })
-    // show => !show
-    setShowResults({ trees })
+    setData({ trees })
+  }
+
+  if (loading) {
+    return (
+      <div className={classes.fullScreenLoader}>
+        <CircularProgress variant='indeterminate' />
+      </div>
+    )
+  }
+
+  if (error) {
+    return (
+      <div className={classes.root}>
+        <Typography variant='h2'>Claytons Trees!</Typography>
+        <Typography variant='subtitle1'>An error occured:</Typography>
+        <Typography variant='body1'>{`${error}`}</Typography>
+      </div>
+    )
   }
 
   return (
-    <Grid container className={classes.root} spacing={2}>
-      <Grid item xs={12}>
-        <Grid container spacing={2}>
-          {data.trees.map(tree => (
-            <Grid key={tree.name} item xs={3}>
-              {showResults
-                ? <Card className={classes.tree_card}>
-                  <CardActionArea>
-                    <CardContent>
-                      <Typography gutterBottom variant='h5' component='h2'>
-                        {tree.name}
-                      </Typography>
-                      <Typography variant='body2' color='textSecondary' component='p'>
-                        {tree.species_name}
-                      </Typography>
-                    </CardContent>
-                    <CardMedia
-                      component='img'
-                      alt={tree.name}
-                      height='600'
-                      image={tree.image}
-                      title={tree.name}
-                    />
-                  </CardActionArea>
-                  <CardActions>
-                    <Button size='small' color='primary' onClick={handleChange}>
-                    Hide Image
-                    </Button>
-                  </CardActions>
-                </Card>
-              // When image is hidden
-                : <Card>
-                  <CardActionArea>
-                    <CardContent>
-                      <Typography gutterBottom variant='h5' component='h2'>
-                        {tree.name}
-                      </Typography>
-                      <Typography variant='body2' color='textSecondary' component='p'>
-                        {tree.species_name}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                  <CardActions>
-                    <Button justify='center' size='small' color='primary' onClick={handleChange}>
-                    Show Image
-                    </Button>
-                  </CardActions>
-                </Card>}
-            </Grid>
-          ))}
-        </Grid>
+    <div className={classes.root}>
+      <Typography variant='h2' gutterBottom>Claytons Trees!</Typography>
+      <Grid container spacing={4}>
+        {isArray(data?.trees) && data.trees.map(tree => (
+          <Grid key={tree.name} item xs={12} sm={6} md={4}>
+            <Card className={classes.treeCard}>
+              <CardActionArea>
+                <CardContent>
+                  <Typography gutterBottom variant='h5' component='h2'>
+                    {tree.name}
+                  </Typography>
+                  <Typography variant='body2' color='textSecondary' component='p'>
+                    {tree.species_name}
+                  </Typography>
+                </CardContent>
+                <Collapse in={!tree.hidden}>
+                  <CardMedia
+                    component='img'
+                    alt={tree.name}
+                    height='600'
+                    image={tree.image}
+                    title={tree.name}
+                  />
+                </Collapse>
+              </CardActionArea>
+              <CardActions className={classes.cardActions}>
+                <Button size='small' color='primary' onClick={() => handleChange(tree.name)}>
+                  Hide Image
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
-    </Grid>
+    </div>
   )
 }
 
